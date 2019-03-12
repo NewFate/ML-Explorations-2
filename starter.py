7f5ccab2c6d94e6379ed71370b683039a2692ca4
shovon77 <shovonmahmud@Shovons-MacBook-Pro-4.local>
2019-03-11 20:27:05 -0400

    Fixed part 1

--- starter.py
+++ starter.py
@@ -71,7 +71,7 @@ def computeLayer(X, W, b):
 
 
 def CE(target, prediction):
-    prediction = softmax(prediction)
+    #prediction = softmax(prediction)
     log_likelihood = np.log(prediction)
     loss = -np.sum(log_likelihood*target) / target.shape[0]
     return loss
@@ -99,7 +99,7 @@ def gradientDescentMomentum(trainData, trainTarget, weight_hidden, weight_output
     v_output = np.ones((hidden_unit_size, 10))*1e-5
     
     lamda = 0.9
-    alpha = 0.0001
+    alpha = 0.00001
     
     #Accuracy need to complete
     accuracy = 0
@@ -145,22 +145,24 @@ def gradientDescentMomentum(trainData, trainTarget, weight_hidden, weight_output
     plt.figure(2)
     plt.plot(accuracy_list, label=hidden_unit_size)
     
-    return y_predict, loss_list, accuracy
+    return y_predict, loss_list, accuracy, weight_hidden, weight_output
 
 
 def backPropagation(trainData, trainTarget, y_predict, hidden_layer_activation, weight_output):
     
     #Calculate the partial derivatives
-    delta_1 = np.transpose(y_predict-trainTarget)
+    delta_1 = np.transpose(y_predict-trainTarget) #10x10000
     #print(delta_1)
     dL_dWo = np.matmul(hidden_layer_activation.transpose(), delta_1.transpose())
     #dL_dWo = np.transpose(dL_dWo) #1000x10
     #print("dL_dWo: ", dL_dWo.shape)
     
     delta_2 = np.multiply(np.matmul(weight_output, delta_1).transpose(), np.sign(hidden_layer_activation)) #10000x1000
+    #print(delta_2.shape)
     #delta_2 = np.transpose(delta_2)
     #print(delta_2)
-    dL_dWh = np.matmul(trainData.transpose(), delta_2) #1000x784
+    dL_dWh = np.matmul(trainData.transpose(), delta_2) #784x1000
+    
     #dL_dWh = np.transpose(dL_dWh)
     #print("dL_dWh: ", dL_dWh.shape)
 
@@ -189,7 +191,87 @@ def frontPropagation(trainData, trainTarget, weight_hidden, weight_output, bias_
     accuracy = find_accuracy(trainTarget, y_predict)
     
     return loss, accuracy, y_predict, hidden_layer_activation
+
+
+    ########End of Neural Network#####
     
+    
+    ########Convolutional Neural Network Functions######
+
+
+def create_convolution_layer(input, num_input_channels, filter_size, num_filters, name):    
+    with tf.variable_scope(name) as scope:
+        # Shape of the filter-weights for the convolution
+        shape = [filter_size, filter_size, num_input_channels, num_filters]
+
+        # Create new weights (filters) with the given shape
+        weights = tf.Variable(tf.truncated_normal(shape, stddev=0.05))
+        
+        #weights = tf.Variable("W", shape=shape, initializer=tf.contrib.layers.xavier_initializer())
+
+        # Create new biases, one for each filter
+        biases = tf.Variable(tf.constant(0.05, shape=[num_filters]))
+
+        # TensorFlow operation for convolution
+        layer = tf.nn.conv2d(input=input, filter=weights, strides=[1, 1, 1, 1], padding='SAME')
+
+        # Add the biases to the results of the convolution.
+        layer += biases
+        
+        return layer, weights
+
+
+def create_pool_layer(input, name):    
+    with tf.variable_scope(name) as scope:
+        # TensorFlow operation for convolution
+        layer = tf.nn.max_pool(value=input, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')        
+        return layer
+    
+    
+    
+def create_relu_layer(input, name):    
+    with tf.variable_scope(name) as scope:
+        # TensorFlow operation for convolution
+        layer = tf.nn.relu(input)
+        
+        return layer
+
+
+def new_fc_layer(input, num_inputs, num_outputs, name):    
+    with tf.variable_scope(name) as scope:
+
+        # Create new weights and biases.
+        weights = tf.Variable(tf.truncated_normal([num_inputs, num_outputs], stddev=0.05))
+        biases = tf.Variable(tf.constant(0.05, shape=[num_outputs]))
+        
+        # Multiply the input and weights, and then add the bias-values.
+        layer = tf.matmul(input, weights) + biases 
+        
+        return layer
+
+
+def create_batch_normalisation_layer(input, name):    
+    with tf.variable_scope(name) as scope:
+        # TensorFlow operation for batch normalisation
+        input_mean, input_var = tf.nn.moments(input,[0])
+        scale = tf.Variable(tf.ones([32]))
+        beta = tf.Variable(tf.zeros([32]))
+        BN = tf.nn.batch_normalization(input, input_mean, input_var, beta, scale, 1e-3)
+        
+        return BN
+
+
+def create_flatten_layer(input, name):    
+    with tf.variable_scope(name) as scope:
+        # TensorFlow operation for flatten
+        layer = tf.layers.flatten(input)
+        
+        return layer
+
+def create_batch(data, batch_size):
+    while data.any():
+        batch, data = data[:batch_size], data[batch_size:]
+        yield batch
 
 def main():
     trainData, validData, testData, trainTarget, validTarget, testTarget = loadData();
@@ -246,10 +328,130 @@ def main():
         #print(b_output)
         
         #Gradient Descent with momentum
-        y_predict, loss_list, accuracy = gradientDescentMomentum(trainData, trainTarget, w_hidden, w_output, b_hidden, b_output, 200, hidden_unit_size)
+        y_predict, loss_list, accuracy, weight_hidden, weight_output = gradientDescentMomentum(trainData, trainTarget, w_hidden, w_output, b_hidden, b_output, 200, hidden_unit_size)
+        valid_loss, accuracy, y_predict, hidden_layer_activation = frontPropagation(validData, validTarget, weight_hidden, weight_output, b_hidden, b_output)
+        print("Test loss: ", valid_loss)
+        test_loss, accuracy, y_predict, hidden_layer_activation = frontPropagation(testData, testTarget, weight_hidden, weight_output, b_hidden, b_output)
+        print("Test loss: ", test_loss)
         print("Finished")
 
     
+    
+    
+    #########################################################
+    #                                                       #
+    #           Convolutional Neural Network                #
+    #                                                       #
+    #                                                       #
+    #########################################################
+    
+    # Placeholder variable for the input images
+    x = tf.placeholder(tf.float32, shape=[None, 28*28], name='X')
+    # Reshape it into [num_images, img_height, img_width, num_channels]
+    x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+    # Placeholder variable for the true labels associated with the images
+    y_true = tf.placeholder(tf.float32, shape=[None, 10], name='y_true')
+    y_true_cls = tf.argmax(y_true, dimension=1)
+    
+    # Convolutional Layer 1
+    layer_conv1, weights_conv1 = create_convolution_layer(input=x_image, num_input_channels=1, filter_size=3, num_filters=32, name ="conv1")
+    
+    # RelU layer 1
+    layer_relu1 = create_relu_layer(layer_conv1, name="relu1")
+    
+    # Batch Normalisation Layer
+    layer_BN = create_batch_normalisation_layer(layer_relu1, name="BN")
+       
+    # Pooling Layer 1
+    layer_pool1 = create_pool_layer(layer_BN, name="pool")
+    
+    # Flatten layer
+    num_features = layer_pool1.get_shape()[1:4].num_elements()
+    layer_flatten = tf.reshape(layer_pool1, [-1, num_features])
+    
+    # Fully-Connected Layer 1
+    layer_fc1 = new_fc_layer(layer_flatten, num_inputs=num_features, num_outputs=784, name="fc1")
+    
+    # RelU layer 2
+    layer_relu2 = create_relu_layer(layer_fc1, name="relu2")
+    
+    # Fully-Connected Layer 2
+    layer_fc2 = new_fc_layer(layer_relu2, num_inputs=784, num_outputs=10, name="fc2")
+      
+    
+    # Use Softmax function to normalize the output
+    with tf.variable_scope("softmax"):
+        y_pred = tf.nn.softmax(layer_fc2)
+        y_pred_cls = tf.argmax(y_pred, dimension=1)
+        
+    
+    # Use Cross entropy cost function
+    with tf.name_scope("cross_entropy"):
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=layer_fc2, labels=y_true)
+        cost = tf.reduce_mean(cross_entropy)
+        
+
+    # Use Adam Optimizer
+    with tf.name_scope("optimizer"):
+        optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
+               
+    # Accuracy
+    with tf.name_scope("accuracy"):
+        correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        
+    num_epochs = 0
+    batch_size = 32
+    train_accuracy_list = []
+    train_loss_list = []
+    
+    with tf.Session() as sess:
+        # Initialize all variables
+        sess.run(tf.global_variables_initializer())
+        
+        # Loop over number of epochs
+        for epoch in range(num_epochs):
+            batch_train_accuracy = []
+            batch_train_loss = [] 
+            
+            # Shuffle trainData and trainTarget
+            trainData, trainTarget = shuffle(trainData, trainTarget)
+            for (batch_x, batch_y) in zip(create_batch(trainData, batch_size), create_batch(trainTarget, batch_size)):
+            
+                #start_time = time.time()
+        
+                
+                # Put the batch into a dict with the proper names for placeholder variables
+                feed_dict_train = {x: batch_x, y_true: batch_y}
+                
+                # Run the optimizer using this batch of training data.
+                sess.run(optimizer, feed_dict=feed_dict_train)
+                
+                # Calculate the accuracy on the batch of training data
+                batch_train_accuracy.append(sess.run(accuracy, feed_dict=feed_dict_train))
+
+                
+                # Calulate the loss on the batch of training data
+                batch_train_loss.append(sess.run(cost, feed_dict=feed_dict_train))
+                #print("Batch Loss: ", train_loss)
+                #print("Batch Accuracy: ", train_accuracy)
+                
+                #print(train_loss_list)
+            
+              
+            train_accuracy_mean = np.mean(batch_train_accuracy)
+            train_loss_mean = np.mean(batch_train_loss)
+
+            
+            train_accuracy_list.append(train_accuracy_mean)
+            train_loss_list.append(train_loss_mean)
+            
+            
+            print("Accuracy: ", train_accuracy_mean, "Loss: ", train_loss_mean)
+                        
+    
+
     
 if __name__ == "__main__":
     main()
